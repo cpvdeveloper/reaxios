0f7daa41232401ee10d330b6512f49c093b8a9a0
cpv123 <c.vibert@hotmail.com>
2019-01-01 19:27:53 +0000

    first commit

---
+++ src/index.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import axios from 'axios';
+
+class Reaxios extends React.PureComponent {
+    
+    constructor(props) {
+        super(props);
+        this.state = {
+            response: null,
+            isLoading: true,
+            error: null,
+        };
+    }
+
+    componentDidMount() {
+        this.makeRequest(this.props);
+    }
+
+    async makeRequest(props) {
+        try {
+            const res = await axios.get(props.url);
+            this.setState({
+                isLoading: false,
+                response: res.data,
+            });
+        } catch(error) {
+            this.setState({
+                error,
+                isLoading: false,
+            })
+        }
+    }
+
+    render() {
+        return this.props.children(this.state);
+    }
+}
+
+Reaxios.propTypes = {
+    children: PropTypes.func,
+    url: PropTypes.string.isRequired,
+};
+
+export default Reaxios;
